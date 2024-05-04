90495e0050ff50b428a2264b36a5cf8d2a314f4c
LauraHPG <laura.hui.perez@estudiantat.upc.edu>
2024-05-04 17:18:36 +0200

    summary page

--- src/views/SummaryPage.vue
+++ src/views/SummaryPage.vue
@@ -3,18 +3,38 @@
 </script>
 <template>
   <div>
-    <div class="flex flex-row">
-      <a href="https://www.google.com/maps"><i class="pi pi-map-marker" style="color: slateblue"></i></a>
-      <div>
+    <div class="flex items-center place-content-around m-5">
+      <a href="https://www.google.com/maps" ><i class="pi pi-map-marker text-3xl" style="color: slateblue"></i></a>
+      <div class="text-center">
 
         <h1 class="font-bold text-2xl">Berlin Cathedral</h1>
         <h3>Berlin, Germany</h3>
       
       </div>
+      <i class="pi pi-check-circle text-3xl text-green-600"></i>
     </div>
     
 
-    <div>User photo</div>
+    <div class="border-solid m-5 border-2 p-2">
+      <h3 class=" text-center text-lg">Photo</h3>
+      
+      <div class="flex justify-center">
+        <button class="border-solid border-2 grow">Upload Photo</button>
+
+      </div>
+      
+    </div>
+
+    <div class="border-solid m-5 border-2 p-2">
+      <h3 class=" text-center text-lg">Information</h3>
+      <p>Visited on:</p>
+      <p>Visited with:</p>
+    </div>
+
+    <div class="border-solid m-5 border-2 p-2">
+      <h3 class="text-center text-lg">Description</h3>
+     
+    </div>
 
   </div>
 </template>
