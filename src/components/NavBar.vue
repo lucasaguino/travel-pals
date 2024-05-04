2a35f20897833a17925715b6ed1a6c1a9dd99fb0
Lucas <lucas.aguino0@gmail.com>
2024-05-04 13:21:49 +0200

    basic setup

---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+import Avatar from 'primevue/avatar'
+
+</script>
+
+<template>
+  <div class="w-100 p-2 flex justify-end items-center">
+    <div>
+      <Avatar image="https://marketplace.canva.com/EAFqNrAJpQs/1/0/1600w/canva-neutral-pink-modern-circle-shape-linkedin-profile-picture-WAhofEY5L1U.jpg" class="mr-2" size="xlarge" shape="circle" />
+    </div>
+    <RouterLink class="font-bold text-lg">Laura</RouterLink>
+
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -5,12 +5,10 @@ import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config'
 import './index.css'
+import 'primevue/resources/themes/saga-blue/theme.css';
 
 const app = createApp(App)
-app.use(
-  PrimeVue, {
-    unstyled: true,
-  }
+app.use(PrimeVue
 )
 
 app.use(router)
