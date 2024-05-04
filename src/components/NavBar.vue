2b9d399b33218bdab5abbc5fa2d9d36fcfa821a4
Lucas <lucas.aguino0@gmail.com>
2024-05-04 15:31:16 +0200

    refined navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,11 +5,14 @@ import Avatar from 'primevue/avatar'
 </script>
 
 <template>
-  <div class="w-100 p-2 flex justify-end items-center">
-    <div>
-      <Avatar image="https://marketplace.canva.com/EAFqNrAJpQs/1/0/1600w/canva-neutral-pink-modern-circle-shape-linkedin-profile-picture-WAhofEY5L1U.jpg" class="mr-2" size="xlarge" shape="circle" />
-    </div>
-    <RouterLink class="font-bold text-lg">Laura</RouterLink>
-
+  <div 
+    :class="['w-100 p-3 flex justify-end items-center',
+      'bg-gradient-to-b from-indigo-500 to-purple-300']"
+  >
+    <Avatar 
+      class="mr-4 shadow-md"
+      image="https://marketplace.canva.com/EAFqNrAJpQs/1/0/1600w/canva-neutral-pink-modern-circle-shape-linkedin-profile-picture-WAhofEY5L1U.jpg" size="xlarge" shape="circle" 
+    />
+    <RouterLink class="font-bold text-black text-lg">Michelle</RouterLink>
   </div>
 </template>
