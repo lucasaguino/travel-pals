f8f2523359876605e4af4d3377350f3dee2191cc
LauraHPG <laura.hui.perez@estudiantat.upc.edu>
2024-05-04 20:17:28 +0200

    testing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import TripInfo from '../components/TripInfo.vue'
+import Button from 'primevue/button';
 
 </script>
 
@@ -12,4 +13,13 @@ import TripInfo from '../components/TripInfo.vue'
       :end-date="'12/03/2024'"
       ></TripInfo>
   </div>
+  <RouterLink to="/tripCreation">
+    <Button label="Add New Trip" />
+  </RouterLink>
+
+    <div class="card flex justify-content-center">
+        <Button label="Submit" />
+    </div>
+
+
 </template>
--- src/views/TripCreationPage.vue
+++ src/views/TripCreationPage.vue
@@ -1,6 +1,31 @@
 <script setup>
 
+import Calendar from 'primevue/calendar';
+import Dropdown from 'primevue/dropdown';
+import Button from 'primevue/button';
+
+
+import { ref } from "vue";
+
+  const selectedCity = ref();
+  const cities = ref([
+      { name: 'New York', code: 'NY' },
+      { name: 'Rome', code: 'RM' },
+      { name: 'London', code: 'LDN' },
+      { name: 'Istanbul', code: 'IST' },
+      { name: 'Paris', code: 'PRS' }
+  ]);
+
+  const selectedCategory = ref();
+  const categories = ref([
+      { name: 'Tourist', code: 'TOUR' },
+      { name: 'Business', code: 'BS' },
+      { name: 'Other', code: 'OTH' }
+  ]);
+
+
 </script>
+
 <template>
   <div>
     <div class="flex items-center place-content-around m-5">
@@ -15,25 +40,42 @@
     <form action="">
         <div class="border-solid m-5 border-2 p-2 flex place-content-between">
             <p>Destination: </p>
-            <input class="border-solid border-2" type="text">
+
+            <div class="card flex justify-content-center">
+                <Dropdown v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select a City" class="w-full md:w-14rem" />
+            </div>
+
         </div>
         <div class="border-solid m-5 border-2 p-2 flex place-content-between ">
             <p>Arrival date: </p>
-            <input type="date">
+            <Calendar v-model="date" />
         </div>
         <div class="border-solid m-5 border-2 p-2 flex place-content-between">
             <p>Departure date: </p>
-            <input type="date">
+            <Calendar v-model="date" />
         </div>
         <div class="border-solid m-5 border-2 p-2 flex place-content-between">
             <p>Category: </p>
-            <input class="border-solid border-2" type="text">
+
+            <div class="card flex justify-content-center">
+                <Dropdown v-model="selectedCategory" :options="categories" optionLabel="name" placeholder="Select a Category" class="w-full md:w-14rem" />
+            </div>
         </div>
-        <div class="border-solid m-5 border-2 p-2 flex place-content-center">
-        
-            <input type="submit" value="Create Trip">
+        <div class="border-solid m-5 border-2 p-2 flex place-content-around">
+          
+          <RouterLink to="/">
+            <Button icon="pi pi-times " style="color : red" raised rounded aria-label="Cancel" />
+          </RouterLink>
+
+          <RouterLink to="/currentTrip">
+            <Button icon="pi pi-check" style="color : green" raised rounded aria-label="Filter" />
+          </RouterLink>
+
         </div>
     </form>
-
   </div>
+
+
+  
 </template>
+
