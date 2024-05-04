9df674c904601cffd1fb8fb996783e4b2b6180a6
LauraHPG <laura.hui.perez@estudiantat.upc.edu>
2024-05-05 01:02:36 +0200

    summary styling

--- src/views/SummaryPage.vue
+++ src/views/SummaryPage.vue
@@ -1,12 +1,9 @@
 <script setup>
-import FileUpload from 'primevue/fileupload';
-
-
 
 </script>
 <template>
   <div>
-    <div class="flex items-center place-content-around m-5">
+    <div class="flex items-center place-content-around !m-5">
       <a href="https://www.google.com/maps" ><i class="pi pi-map-marker text-3xl" style="color: slateblue"></i></a>
       <div class="text-center">
 
@@ -18,32 +15,24 @@ import FileUpload from 'primevue/fileupload';
     </div>
     
 
-      
-      
-    <Card style="margin: 10px;">
-        <template #title>Photo</template>
-        <template #content>
+      <Card class="bg-cover bg-center m-10 min-h-60 rounded-md" style="margin: 10px; background-image: url(https://www.berlin.de/binaries/asset/image_assets/6431736/source/1643977486/1000x500/);">
+        
+      </Card>
 
-    <div class="card flex justify-content-center">
-        <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStSksdSGqrVSipcqrmyQk4tLjsiTWw-x2vga6mw438gw&s" alt="Image" width="250" />
-    </div>
 
-          
-        </template>
-    </Card>
-
-    <Card style="margin: 10px;">
-        <template #title>Information</template>
+    <Card class="rounded-md" style="margin: 10px;">
         <template #content>
           <p>Visited on:</p>
           <p>Visited with:</p>
         </template>
     </Card>
 
-    <div class="border-solid m-5 border-2 p-2">
-      <h3 class="text-center text-lg">Description</h3>
-     
-    </div>
+    <Card class="rounded-md" style="margin: 10px;">
+      <template #title>Description</template>
+      <template #content>
+        <p>wiki description</p>
+      </template>
+    </Card>
 
   </div>
 </template>
