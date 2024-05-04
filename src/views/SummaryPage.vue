7a13a4defdc5caadff661dab22c37c5f4d1262ff
LauraHPG <laura.hui.perez@estudiantat.upc.edu>
2024-05-04 16:08:42 +0200

    testing

--- src/views/SummaryPage.vue
+++ src/views/SummaryPage.vue
@@ -3,6 +3,18 @@
 </script>
 <template>
   <div>
-    <p>Página summary</p>
+    <div class="flex flex-row">
+      <a href="https://www.google.com/maps"><i class="pi pi-map-marker" style="color: slateblue"></i></a>
+      <div>
+
+        <h1 class="font-bold text-2xl">Berlin Cathedral</h1>
+        <h3>Berlin, Germany</h3>
+      
+      </div>
+    </div>
+    
+
+    <div>User photo</div>
+
   </div>
 </template>
