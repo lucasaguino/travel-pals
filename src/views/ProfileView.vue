eef7081380bcdc11351782b969c31d7192d74a20
LauraHPG <laura.hui.perez@estudiantat.upc.edu>
2024-05-05 07:59:56 +0200

    paulita

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -22,7 +22,7 @@ const events = user.trips.map((el) => ({ ...styleInfo, ...el}))
 <template>
   <div
     class="bg-cover relative bg-center h-[20rem]"
-    style="background-image: url(https://marketplace.canva.com/EAFqNrAJpQs/1/0/1600w/canva-neutral-pink-modern-circle-shape-linkedin-profile-picture-WAhofEY5L1U.jpg);"
+    style="background-image: url(/src/assets/photos/6/profile.jpg);"
   >
     <div
       class="absolute w-full h-[5rem] bottom-0 bg-gradient-to-b from-white/0 to-white/100"
@@ -31,8 +31,8 @@ const events = user.trips.map((el) => ({ ...styleInfo, ...el}))
   </div>
   <div>
     <header class="flex flex-col items-center p-4">
-      <h3 class="text-5xl text-center">{{ user.name }}</h3>
-      <h5 class="!mt-3 text-2xl">Expert Traveller</h5>
+      <h3 class="text-5xl text-center">Paula Hays</h3>
+      <h5 class="!mt-3 text-2xl">Beginner Traveller</h5>
       <p class="!mt-3">Visited <span class="!font-bold text-xl">22</span> cities and <span class="!font-bold text-xl">5</span> countries</p>
     </header>
   </div>
@@ -45,16 +45,29 @@ const events = user.trips.map((el) => ({ ...styleInfo, ...el}))
       :end-date="'12/03/2024'"
       ></TripInfo>
   </div>
+
   <div class="!p-5 ">
     <h2 class="!font-bold !text-2xl !mb-6">Your past trips</h2>
     <TripInfo 
-      :place="'Barcelona'"
-      :country="'Spain'"
-      :start-date="'02/03/2024'"
-      :end-date="'12/03/2024'"
+      :place="'Cairo'"
+      :country="'Egypt'"
+      :start-date="'12/02/2024'"
+      :end-date="'22/02/2024'"
+      completed
+      ></TripInfo>
+
+      <div class="!my-5"></div>
+
+      <TripInfo 
+      :place="'London'"
+      :country="'United Kingdom'"
+      :start-date="'23/02/2024'"
+      :end-date="'03r/03/2024'"
       completed
       ></TripInfo>
   </div>
+  
+
 </template>
 
 <style>
